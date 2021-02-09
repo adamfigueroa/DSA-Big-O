027c600c22c523df2e84ae64ef3b5fc656c498a0
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-09 09:13:14 -0800

    question 8 complete

---
+++ Question-8.js
@@ -0,0 +1,26 @@
+// In this example, we return to the problem of searching using a more sophisticated approach than in naive search, above. Assume that the input array is always sorted. What is the Big O of the following algorithm? Explain your answer
+
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+// Logarithmic O(log(n)), since we know that the array is sorted before, this function can split the array in 2 and if the current element is less than( or greater than) the item we are looking for, the we can eliminate a large amount of data.
