31757f0621578d7f6c04afe92f658dcc2c62611f
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-09 08:52:48 -0800

    question 5 complete

---
+++ Question-5.js
@@ -0,0 +1,11 @@
+// What is the Big O of the following algorithm? Explain your answer
+
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+
+// Linear O(n), n because you are looping through each item in the array but only performing a check on the item
